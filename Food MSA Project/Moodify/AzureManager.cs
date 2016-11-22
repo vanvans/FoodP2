8ba0f7971b1d902b9fb746ade4bd57dfd626e4d8
Vanessa <vwon364@aucklanduni.ac.nz>
2016-11-23 04:32:29 +1300

    integrating database

---
+++ Food MSA Project/Moodify/AzureManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+
+namespace Moodify
+{
+    public class AzureManager
+    {
+
+        private static AzureManager instance;
+        private MobileServiceClient client;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("https://giraffes.azurewebsites.net/");
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
---
+++ Food MSA Project/Moodify/DataModels/fakedata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moodify.DataModels
+{
+    public class fakedata
+    {
+        public string id { get; set; }
+        public string createdAt { get; set; }
+        public string updatedAt { get; set; }
+        public string version { get; set; }
+        public string MenuID { get; set; }
+        public string ItemName { get; set; }
+        public string MenuDesc { get; set; }
+        public string Category { get; set; }
+        public string Price { get; set; }
+        public string Promo { get; set; }
+        public string PromoDiscount { get; set; }
+        public string OrderNo { get; set; }
+        public string CustomerName { get; set; }
+        public string ImageF { get; set; }
+        public bool deleted { get; set; }
+    }
+
+}
