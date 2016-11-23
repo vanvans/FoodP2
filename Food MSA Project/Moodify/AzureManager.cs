d06d076d5d0e63809f80b3d9fa320f74f4830541
Vanessa <vwon364@aucklanduni.ac.nz>
2016-11-23 17:37:04 +1300

    fixed errors

--- Food MSA Project/Moodify/AzureManager.cs
+++ Food MSA Project/Moodify/AzureManager.cs
@@ -1,22 +1,24 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Moodify.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.WindowsAzure.MobileServices;
 
 
 namespace Moodify
 {
-    public class AzureManager
+    class AzureManager
     {
-
         private static AzureManager instance;
         private MobileServiceClient client;
+        private IMobileServiceTable<Fakedata> fakedataTable;
 
         private AzureManager()
         {
-            this.client = new MobileServiceClient("https://giraffes.azurewebsites.net/");
+            this.client = new MobileServiceClient("http://giraffes.azurewebsites.net");
+            this.fakedataTable = this.client.GetTable<Fakedata>();
         }
 
         public MobileServiceClient AzureClient
@@ -32,9 +34,20 @@ namespace Moodify
                 {
                     instance = new AzureManager();
                 }
-
                 return instance;
             }
         }
+
+        public async Task AddFakedata(Fakedata fakedata)
+        {
+            await this.fakedataTable.InsertAsync(fakedata);
+        }
+
+        public async Task<List<Fakedata>> GetFakedatas()
+        {
+            return await this.fakedataTable.ToListAsync();
+        }
+
+
     }
 }
--- Food MSA Project/Moodify/DataModels/fakedata.cs
+++ Food MSA Project/Moodify/DataModels/fakedata.cs
@@ -3,26 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Moodify.DataModels
 {
-    public class fakedata
+    public class Fakedata
     {
-        public string id { get; set; }
-        public string createdAt { get; set; }
-        public string updatedAt { get; set; }
-        public string version { get; set; }
+        [JsonProperty(PropertyName="MenuID")]
         public string MenuID { get; set; }
+
+        [JsonProperty(PropertyName = "itemName")]
         public string ItemName { get; set; }
+
+        [JsonProperty(PropertyName = "MenuDesc")]
         public string MenuDesc { get; set; }
+
+        [JsonProperty(PropertyName = "category")]
         public string Category { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
         public string Price { get; set; }
+
+        [JsonProperty(PropertyName = "promo")]
         public string Promo { get; set; }
+
+        [JsonProperty(PropertyName = "promoDisc")]
         public string PromoDiscount { get; set; }
+
+        [JsonProperty(PropertyName = "orderNo")]
         public string OrderNo { get; set; }
+
+        [JsonProperty(PropertyName = "custName")]
         public string CustomerName { get; set; }
+
+        [JsonProperty(PropertyName = "imagef")]
         public string ImageF { get; set; }
+
+        [JsonProperty(PropertyName = "deleted")]
         public bool deleted { get; set; }
+   
     }
 
 }
